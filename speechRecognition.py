3fa1341a8c28e69a38ef104d12c48143a510974a
sbaglieri13 <totobaglieri95@gmail.com>
2021-08-06 19:07:00 +0200

    Update speech recognition

--- speechRecognition.py
+++ speechRecognition.py
@@ -1,30 +1,19 @@
 import speech_recognition as sr
 
 
-# Performs speech recognition on audio_data (an AudioData instance), using the Google Speech Recognition API. The
-# Google Speech Recognition API key is specified by key. If not specified, it uses a generic key that works out of
-# the box. This should generally be used for personal or testing purposes only, as it may be revoked by Google at any
-# time.
-def run():
-    # create a speech recognition object
+def run(audio_file):
+    # use the audio file as the audio source
     recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        # obtain audio from the microphone
-        print("Say something!")
-        recognizer.pause_threshold = 0.7
-        audio_data = recognizer.listen(source)
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)  # read the entire audio file
 
-    # recognize speech using Google Speech Recognition
+    # recognize speech using Sphinx
     try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        text = recognizer.recognize_google(audio_data)
-
+        text = recognizer.recognize_sphinx(audio)
         return text
-
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        print("Sphinx could not understand audio")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Sphinx error; {0}".format(e))
+
